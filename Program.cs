c5a9e029edc1cdaf8de3f0151c56ea9e9dd2bc1f
Your Name <you@example.com>
2020-06-16 15:17:03 -0300

    Aula18Dojo

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Aula18Dojo1
+{
+    class Program
+    {
+             
+        static void Main(string[] args)
+        {
+
+            Jogador lucca = new Jogador();
+            lucca.Nome= "Lucca";
+            lucca.Altura = 1.78f;
+            lucca.Peso = 68f;
+            lucca.Nascimento = DateTime.Parse("20/11/2020");
+            lucca.Posicao = "Goleiro";
+            
+            Console.WriteLine(lucca.CalcularIdade());
+            Console.WriteLine(lucca.VerificarAposentadoria());
+            
+        }
+    
+        
+    }
+}
